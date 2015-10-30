d11669dc7d5fbb76bf43318e9bfa6b9af7154088
rexcode <vickympatel@gmail.com>
2015-10-31 02:24:23 +0530

    paperclip s3 optional

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,4 +2,4 @@
  Paperclip::Attachment.default_options[:path] = 'class/:attachment/:id_partition/:style/:filename'
 
  # optional
- # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+ Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
